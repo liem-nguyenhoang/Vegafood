bb64c8c36e27a0a5c4d81cf15f1442376deecb21
LiemNH7 <liemnh7@fpt.com.vn>
2021-01-11 22:42:18 +0700

    Show tên của product

--- src/components/menu/detailMenuOrder.js
+++ src/components/menu/detailMenuOrder.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './detailMenuOrder.css';
 import {
     Link,
     useParams
 } from "react-router-dom";
 
 export default function DetailMenuOrder() {
-
     let { id } = useParams()
 
+    const [listIdProduct, setlistIdProduct] = useState([])
+    const [listProduct, setlistProduct] = useState([])
+
+    useEffect(() => {
+        fetch('https://localhost:44323/api/OrderDetails/' + id)
+            .then(response => response.json())
+            .then(data => {
+                setlistIdProduct(data)
+            })
+    }, [])
+
+    useEffect(() => {
+        listIdProduct.map((value) => {
+            fetch('https://localhost:44323/api/Products/' + value.productId)
+                .then(response => response.json())
+                .then(data => {
+                    setlistProduct(old => [...old, data])
+                })
+        })
+
+    }, [listIdProduct])
     return (
-        <div>
+        <div className="dsad">
             <a><Link to="/">AppHome</Link></a>
             <div>id: {id}</div>
-        </div>
+
+            {listProduct.map((value, index) => {
+                return (
+                    <h1>{value.productName}</h1>
+                )
+            })}
+        </div >
     )
 }
